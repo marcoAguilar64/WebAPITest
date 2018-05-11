3fbf717c8a89649fd056d35fc6217ee806f71840
Marco Antonio Aguilar Zambrana <marco.aguilarz@gmail.com>
2018-05-10 21:38:58 -0400

    mi primer commit

---
+++ TodoApi/Startup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using TodoApi.Models;
+
+namespace TodoApi
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<TodoContext>(opt => opt.UseInMemoryDatabase());
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {          
+            app.UseMvc();
+        }
+    }
+}
